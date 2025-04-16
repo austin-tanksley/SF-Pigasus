79b2c0184dd983c321ac53034e97a7f769f392b1
Austin Tanksley <58612782+austin-tanksley@users.noreply.github.com>
2025-04-16 17:10:20 -0500

    initial setup

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 15, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -11,8 +11,9 @@ renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshPhongMaterial( { color: 0x00ff00} );
 const cube = new THREE.Mesh( geometry, material );
+// cube.position.x = -2;
 scene.add( cube );
 
 camera.position.z = 5;
