077d15aa9a66646748c94a6aabb03b3eafda9085
Austin Tanksley <58612782+austin-tanksley@users.noreply.github.com>
2025-04-17 16:41:44 -0500

    initial setup

--- src/main.js
+++ src/main.js
@@ -1,28 +1,36 @@
 import "./styles.css";
+// import porky from "../public/Signal_PIG.gltf"
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
+const canvas = document.querySelector("canvas.webGL")
+// const loader = new GLTFLoader();
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 15, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshPhongMaterial( { color: 0x00ff00} );
-const cube = new THREE.Mesh( geometry, material );
-// cube.position.x = -2;
-scene.add( cube );
+const camera = new THREE.PerspectiveCamera( 35, sizes.width / sizes.height, 0.1, 1000 );
+scene.add(camera)
 
 camera.position.z = 5;
+//objects
 
-function animate() {
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+//renderer
+const renderer = new THREE.WebGLRenderer(
+  {
+    canvas: canvas
+  }
+);
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
 
-  renderer.render( scene, camera );
 
-}
