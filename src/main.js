9ba20f21d81d44e5f9738839eb39b277d1717afe
Austin Tanksley <58612782+austin-tanksley@users.noreply.github.com>
2025-04-23 17:41:07 -0500

    lighting and some texture

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import "./styles.css";
 import * as THREE from 'three';
-// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+// import { RectAreaLightHelper } from 'three/addons/helpers/RectAreaLightHelper.js';
+import materials from './materials.js'
+
 
 // 
 const sizes = {
@@ -8,23 +12,76 @@ const sizes = {
   height: window.innerHeight
 }
 
+
 const canvas = document.querySelector("canvas.webGL")
-// const loader = new GLTFLoader();
 
 //scene + camera
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 35, sizes.width / sizes.height, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 25, sizes.width / sizes.height, 0.1, 1000 );
 scene.add(camera)
 
-camera.position.z = 5;
+camera.position.z = 4 ;
+camera.position.y = 3;
+camera.position.x = 2;
+const controls = new OrbitControls(camera, canvas);
+controls.target.y = .75;
+controls.enableDamping = true;
+
+//lights
+const ambient = new THREE.AmbientLight(0xffffff, 2)
+
+const point_light_01 = new THREE.PointLight(0xffffff, 1);
+point_light_01.position.x = -.5;
+point_light_01.position.y = 2;
+point_light_01.position.z = -.2;
+scene.add(point_light_01, ambient)
+const helper_01 = new THREE.PointLightHelper( point_light_01, .2 );
+// scene.add( helper_01 );
+
+const point_light_02 = new THREE.PointLight(0xffffff, 2);
+point_light_02.position.set(-2,.4,.4)
+scene.add(point_light_02)
+const helper_02 = new THREE.PointLightHelper( point_light_02, .2 );
+// scene.add( helper_02 );
+
+const point_light_03 = new THREE.PointLight(0xffffff, 2);
+point_light_03.position.set(1,.6,-1)
+scene.add(point_light_03)
+const helper_03 = new THREE.PointLightHelper( point_light_03, .2 );
+// scene.add( helper_03 );
+
+const spot_light = new THREE.SpotLight(0xffffff, 8);
+spot_light.position.set(1,3,1);
+spot_light.lookAt( 0, 1, 0);
+const spot_helper = new THREE.SpotLightHelper(spot_light);
+
+scene.add(spot_light);
+
+//models
+const pig_mat = materials.gold_paint;
+const gltfLoader = new GLTFLoader()
+gltfLoader.load(
+  '/pigasus.glb',
+  (gltf) => {
+    console.log(gltf)
+    gltf.scene.children[0].children[0].material = pig_mat;
+    gltf.scene.children[0].children[0].map = pig_mat;
+    scene.add(gltf.scene.children[0])
+  },
+  () => {
+    console.log('progress')
+  }
+)
+
+const floor_mat = materials.floor_mat;
 
 
-//objects
-
-const geometry = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const plane = new THREE.Mesh(
+new THREE.PlaneGeometry(40,40,22,22), 
+floor_mat
+);
+plane.rotation.x = -(Math.PI * .5)
+scene.add(plane);
 
 //renderer
 const renderer = new THREE.WebGLRenderer(
@@ -33,6 +90,13 @@ const renderer = new THREE.WebGLRenderer(
   }
 );
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+// renderer.render(scene, camera)
 
 
+const frame = () => {
+
+  controls.update();
+  renderer.render(scene, camera);
+  requestAnimationFrame(frame);
+}
+frame()
---
+++ src/materials.js
@@ -0,0 +1,40 @@
+import * as THREE from 'three'
+
+export default (() => {
+
+    const tiling = {
+        floor: {
+            u: 20,
+            v: 20
+        }
+    }
+    const texture_loader = new THREE.TextureLoader()
+    const gold_paint_color = texture_loader.load('/textures/gold_paint/gold_paint_color.jpg');
+    gold_paint_color.colorSpace = THREE.SRGBColorSpace
+
+    const floor_roughness = texture_loader.load('/textures/floor/floor_roughness.jpg');
+    floor_roughness.repeat.set(tiling.floor.u, tiling.floor.v);
+    floor_roughness.wrapS = THREE.RepeatWrapping;
+    floor_roughness.wrapT = THREE.RepeatWrapping;
+
+    const floor_normal = texture_loader.load('/textures/floor/floor_normal.png');
+    floor_normal.repeat.set(tiling.floor.u, tiling.floor.v);
+    floor_normal.wrapS = THREE.RepeatWrapping;
+    floor_normal.wrapT = THREE.RepeatWrapping;
+    const gold_paint = new THREE.MeshStandardMaterial({
+        map: gold_paint_color,
+        metalness: 1,
+        roughness: 1.5,
+        roughnessMap: texture_loader.load('/textures/gold_paint/gold_paint_roughness.jpg'),
+        normalMap: texture_loader.load('/textures/gold_paint/gold_paint_normal.png')
+    });
+    const floor_mat = new THREE.MeshStandardMaterial({
+        color: 0x151515,
+        metalness: .7,
+        roughnessMap: floor_roughness,
+        normalMap: floor_normal
+    });
+
+    return {gold_paint, floor_mat}
+})();
+
